9781b32fbeff2e692a88f072b0ba38e730695422
Eszter Kovacs <ekov@protonmail.ch>
2017-06-06 16:32:17 +0200

    add basic template for weather

--- weather.py
+++ weather.py
@@ -2,11 +2,6 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return render_template('index.html')
-
-@app.route('/w')
-def get_weather(coords=None):
-    if coords == None:
-        coords = [0, 0]
-    return 'How\'s the weather in {} today?'.format(coords)
+def get_weather():
+    weather = {'summary': 'windy', 'temperature': 29}
+    return render_template('current_weather.html', weather=weather)
