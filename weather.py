658aad04902bb62fe174a072851b32e65174b9e1
Eszter <ekov@protonmail.ch>
2017-06-06 23:00:27 +0200

    get_coords for location, not working

--- weather.py
+++ weather.py
@@ -1,8 +1,29 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+import json
+from config import *
+
 app = Flask(__name__)
+app.config.update(dev_env)
+
+API_KEY = app.config['DARKSKY_API_KEY']
 
 weather = {'summary': 'windy', 'temperature': 29}
 
 @app.route('/')
 def get_weather(location='Budapest'):
+    latitude, longitude = get_coords_for_location(location)
+    weather = get_current_weather(latitude, longitude)
     return render_template('current_weather.html', location=location, weather=weather)
+
+def get_coords_for_location(location):
+    if location == 'Budapest':
+        return (47.48974156155466, 19.054009282892107)
+    return None
+
+def get_current_weather(latitude, longitude):
+    api_url = 'https://api.darksky.net/forecast/{}/{},{}'.format(API_KEY, latitude, longitude)
+    res = json.loads(api_url)
+    # return 'https://api.darksky.net/forecast/{}/{},{}'.format(API_KEY, latitude, longitude)
+    # return {'summary': 'windy', 'temperature': 29}
+    print(res)
+    return None
--- weather_tests.py
+++ weather_tests.py
@@ -6,12 +6,14 @@ class WeatherTestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = weather.app.test_client()
+        self.API_KEY = weather.app.config['DARKSKY_API_KEY']
 
     def tearDown(self):
         pass
 
     def test_weather_gets_location(self):
-        location = 'Budapest'
+        latitude = 47.48974156155466
+        longitude = 19.054009282892107
         rv = self.app.get('/')
         assert b'Budapest' in rv.data
 
@@ -21,6 +23,17 @@ class WeatherTestCase(unittest.TestCase):
         assert b'sunny' not in rv.data
         assert b'windy' in rv.data
         assert b'29' in rv.data
+
+    def test_get_coords_for_location(self):
+        good_location = 'Budapest'
+        bad_location = 'NotReallyBudapest'
+        assert weather.get_coords_for_location(good_location) == (47.48974156155466, 19.054009282892107)
+        assert weather.get_coords_for_location(bad_location) != (47.48974156155466, 19.054009282892107)
+
+    def test_get_current_weather_calls_correct_API_endpoint(self):
+        latitude = 47.489
+        longitude = 19.054
+        assert weather.get_current_weather(latitude, longitude) == 'https://api.darksky.net/forecast/' + self.API_KEY + '/47.489,19.054'
 
 if __name__ == '__main__':
     unittest.main()
