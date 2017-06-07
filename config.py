f3fb176a542dfac791a9812727a3972be4c4be9f
Eszter <ekov@protonmail.ch>
2017-06-07 23:29:12 +0200

    modify config;py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
 prod_env = dict(
-    DARKSKY_API_KEY=os.environ.get('API_KEY'),
-	SECRET_KEY=os.environ.get('SECRET_KEY')
+    DARKSKY_API_KEY=os.environ.get('API_KEY')
+)
+
+dev_env = dict(
+    DARKSKY_API_KEY=os.environ.get('API_KEY')
 )
