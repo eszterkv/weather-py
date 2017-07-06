d0935e2a3ae9b8289a7acb5a4f4c7d803e946f50
Eszter <ekov@protonmail.ch>
2017-07-06 21:39:49 +0200

    refactor tests

--- weather_tests.py
+++ weather_tests.py
@@ -19,51 +19,40 @@ class WeatherTestCase(unittest.TestCase):
         rv = self.app.get('/')
         assert b'Budapest' in rv.data
 
+    def test_get_units_in_celsius_when_in_hungary(self):
+        coords = (47.489, 19.054)
+        assert weather.get_weather_data(coords)['flags']['units'] == 'si'
+
     def test_get_coords_for_location(self):
         good_location = 'Budapest'
         bad_location = 'Southampton'
         not_a_location = 'ReallyNoPlaceCalledHere'
-        print(weather.get_coords_for_location(good_location))
         assert weather.get_coords_for_location(good_location) == (47.49801, 19.03991)
         assert weather.get_coords_for_location(bad_location) != (47.49801, 19.03991)
         assert weather.get_coords_for_location(not_a_location) != (47.49801, 19.03991)
         assert weather.get_coords_for_location(not_a_location) == None
 
+    def test_get_weather_data(self):
+        coords = (47.489, 19.054)
+
+        expected_weather_data_keys = set([u'hourly', u'currently', u'longitude', u'flags',
+                                            u'daily', u'offset', u'latitude', u'timezone'])
+        assert set(weather.get_weather_data(coords).keys()) == expected_weather_data_keys
+
+        expected_daily_keys = set([u'summary', u'icon', u'data'])
+        assert set(weather.get_weather_data(coords)['daily'].keys()) == expected_daily_keys
+
+        expected_weekly_keys = set([u'time', u'icon', u'temperatureMin', u'temperatureMax'])
+        assert len(weather.get_weather_data(coords)['daily']['data']) == 8
+        assert expected_weekly_keys <= set(weather.get_weather_data(coords)['daily']['data'][0].keys())
+
     def test_get_current_weather(self):
         coords = (47.489, 19.054)
         keys = set(weather.get_current_weather(coords).keys())
         expected_keys = set(['summary', 'temperature', 'feels_like', 'icon',
-                            'daily_forecast', 'daily_min_temp', 'daily_max_temp'])
-        expected_keys_with_alerts = set(['summary', 'temperature', 'feels_like', 'icon',
-                                        'daily_forecast', 'daily_min_temp', 'daily_max_temp', 'alerts'])
-        assert keys == expected_keys or keys == expected_keys_with_alerts
+                                'daily_forecast', 'daily_min_temp', 'daily_max_temp', 'alerts'])
+        assert keys <= expected_keys
 
-    def test_get_weather_data(self):
-        coords = (47.489, 19.054)
-        expected_keys = [u'hourly', u'currently', u'longitude', u'flags', u'daily', u'offset', u'latitude', u'timezone']
-        assert weather.get_weather_data(coords).keys() == expected_keys
-
-    def test_get_daily_forecast(self):
-        coords = (47.489, 19.054)
-        expected_keys = set([u'summary', u'icon', u'data'])
-        assert set(weather.get_weather_data(coords)['daily'].keys()) == expected_keys
-
-    def test_get_units_in_celsius_when_in_hungary(self):
-        coords = (47.489, 19.054)
-        assert weather.get_weather_data(coords)['flags']['units'] == 'si'
-
-    @unittest.skip('should use mock requests')
-    @mock.patch('weather.')
-    def test_weather_view_gets_data(self):
-        location = 'Budapest'
-        current_weather = {
-            'summary': 'Sunny Weather Test',
-            'temperature': int(round(13.4)),
-            'feels_like': int(round(15.9)),
-        }
-        rv = weather.get_weather(location=location)
-        print(rv.data)
-        assert 'superweather' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
