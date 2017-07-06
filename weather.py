5c444a992b30280b86573a1e0706e05c0b79efae
Eszter <ekov@protonmail.ch>
2017-07-06 21:50:50 +0200

    reduce nr of api calls in get_weather, improve naming

--- weather.py
+++ weather.py
@@ -18,34 +18,36 @@ API_KEY = app.config['DARKSKY_API_KEY']
 @app.route('/')
 def get_weather(location='Budapest'): # FIXME get user's location
     coords = get_coords_for_location(location)
-    return render_template('current_weather.html', location=location, weather=get_current_weather(coords))
+    return render_template('current_weather.html', location=location, weather=get_weather(coords))
 
 def get_coords_for_location(location_name):
     location = geo.geocode(location_name)
     return (location.latitude, location.longitude) if location else None
 
-def get_weather_data(coords):
+def get_weather_data_from_api(coords):
     latitude, longitude = coords
     api_url = 'https://api.darksky.net/forecast/{}/{},{}?units=auto'.format(API_KEY, latitude, longitude)
     res = requests.get(api_url)
     return res.json()
 
-def get_current_weather(coords):
-    current_data = get_weather_data(coords).get('currently')
-    daily_data = get_weather_data(coords).get('daily')
-    daily_forecast = daily_data.get('data')[0]
-    alerts = get_weather_data(coords).get('alerts')
-    current_weather = {
-        'summary': current_data.get('summary'),
-        'temperature': int(round(current_data.get('temperature'))),
-        'feels_like': int(round(current_data.get('apparentTemperature'))),
-        'icon': current_data.get('icon'),
-        'daily_forecast': daily_forecast.get('summary'),
-        'daily_min_temp': int(round(daily_forecast.get('temperatureMin'))),
-        'daily_max_temp': int(round(daily_forecast.get('temperatureMax'))),
+def get_weather(coords):
+    weather_data = get_weather_data_from_api(coords)
+    weather_now = weather_data.get('currently')
+    daily_data = weather_data.get('daily')
+    weather_today = daily_data.get('data')[0]
+    alerts = weather_data.get('alerts')
+    weather = {
+        'summary': weather_now.get('summary'),
+        'temperature': int(round(weather_now.get('temperature'))),
+        'feels_like': int(round(weather_now.get('apparentTemperature'))),
+        'icon': weather_now.get('icon'),
+        'daily_forecast': weather_today.get('summary'),
+        'daily_min_temp': int(round(weather_today.get('temperatureMin'))),
+        'daily_max_temp': int(round(weather_today.get('temperatureMax'))),
         'alerts': [alert.get('title') for alert in alerts] if alerts else None,
     }
-    return current_weather
+    return weather
+
 
 if __name__ == '__main__':
 	with app.app_context():
--- weather_tests.py
+++ weather_tests.py
@@ -21,7 +21,7 @@ class WeatherTestCase(unittest.TestCase):
 
     def test_get_units_in_celsius_when_in_hungary(self):
         coords = (47.489, 19.054)
-        assert weather.get_weather_data(coords)['flags']['units'] == 'si'
+        assert weather.get_weather_data_from_api(coords)['flags']['units'] == 'si'
 
     def test_get_coords_for_location(self):
         good_location = 'Budapest'
@@ -32,23 +32,23 @@ class WeatherTestCase(unittest.TestCase):
         assert weather.get_coords_for_location(not_a_location) != (47.49801, 19.03991)
         assert weather.get_coords_for_location(not_a_location) == None
 
-    def test_get_weather_data(self):
+    def test_get_weather_data_from_api(self):
         coords = (47.489, 19.054)
 
         expected_weather_data_keys = set([u'hourly', u'currently', u'longitude', u'flags',
                                             u'daily', u'offset', u'latitude', u'timezone'])
-        assert set(weather.get_weather_data(coords).keys()) == expected_weather_data_keys
+        assert set(weather.get_weather_data_from_api(coords).keys()) == expected_weather_data_keys
 
         expected_daily_keys = set([u'summary', u'icon', u'data'])
-        assert set(weather.get_weather_data(coords)['daily'].keys()) == expected_daily_keys
+        assert set(weather.get_weather_data_from_api(coords)['daily'].keys()) == expected_daily_keys
 
         expected_weekly_keys = set([u'time', u'icon', u'temperatureMin', u'temperatureMax'])
-        assert len(weather.get_weather_data(coords)['daily']['data']) == 8
-        assert expected_weekly_keys <= set(weather.get_weather_data(coords)['daily']['data'][0].keys())
+        assert len(weather.get_weather_data_from_api(coords)['daily']['data']) == 8
+        assert expected_weekly_keys <= set(weather.get_weather_data_from_api(coords)['daily']['data'][0].keys())
 
     def test_get_current_weather(self):
         coords = (47.489, 19.054)
-        keys = set(weather.get_current_weather(coords).keys())
+        keys = set(weather.get_weather(coords).keys())
         expected_keys = set(['summary', 'temperature', 'feels_like', 'icon',
                                 'daily_forecast', 'daily_min_temp', 'daily_max_temp', 'alerts'])
         assert keys <= expected_keys
