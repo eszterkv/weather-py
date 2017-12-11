e8165ae3ae6360eaa112d80ffddc6a1ee5e485b8
Eszter Kovacs <ekov@protonmail.ch>
2017-12-11 11:46:12 +0100

    rm location detection

--- static/scripts/utils.js
+++ static/scripts/utils.js
@@ -1,8 +1,8 @@
 window.onload = () => {
-  navigator.geolocation && navigator.geolocation.getCurrentPosition((pos) =>
-    console.log(pos.coords.latitude, pos.coords.longitude)
-  );
-  locationDetector.submit();
+  // navigator.geolocation && navigator.geolocation.getCurrentPosition((pos) =>
+  //   console.log(pos.coords.latitude, pos.coords.longitude)
+  // );
+  // locationDetector.submit();
 
   const locationChooser = document.querySelector('#locationChooser');
   locationChooser.onkeypress = (key) => {
